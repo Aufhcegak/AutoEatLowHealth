52401a6c3a30ec6e60252f92211ed10bd8a78dfb
Shege <112167215@qq.com>
2026-08-04 15:39:27 +0800

    Update src/ModConfig.cs

--- src/ModConfig.cs
+++ src/ModConfig.cs
@@ -9,7 +9,7 @@ public class ModConfig
     public bool InvincibleWhilePrompt { get; set; } = true;
 
     /// <summary>两次触发的最短间隔(秒)。</summary>
-    public int CooldownSeconds { get; set; } = 8;
+    public int CooldownSeconds { get; set; } = 5;
 
     /// <summary>进食优先级(QualifiedItemId,越靠前越先吃)。</summary>
     public List<string> FoodPriority { get; set; } = new();
